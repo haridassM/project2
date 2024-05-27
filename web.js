388241dce46dc36b186b0fc3ed41775da7489cde
haridassM <hdass591@gmail.com>
2024-05-27 17:10:16 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,101 @@
+
+let mydiv=document.getElementById("but1")
+mydiv.addEventListener("click",()=>{
+   
+// let but=document.getElementById("but1")
+let acct=document.getElementById("art")
+acct.style.display="block"
+acct.style.opacity="0.7 "
+let hed=document.getElementById("header1")
+hed.style.backgroundColor="white"
+
+
+
+ 
+    
+
+})
+let mydiv2=document.getElementById("uniq1")
+mydiv2.addEventListener("click",()=>{
+
+let first=document.getElementById("dog1")
+let second=document.getElementById("cat1")
+let third=document.getElementById("dog")
+let four=document.getElementById("cat")
+
+second.style.display="none"
+first.style.display="block"
+
+
+
+
+
+})
+let mydiv3=document.getElementById("uniq2")
+mydiv3.addEventListener("click",()=>{
+
+let first=document.getElementById("dog1")
+let second=document.getElementById("cat1")
+let third=document.getElementById("dog")
+let four=document.getElementById("cat")
+
+second.style.display="block"
+
+
+first.style.display="none"
+
+
+
+
+})
+
+
+
+let mydiv4=document.getElementById("uniq1")
+mydiv4.addEventListener("click",()=>{
+
+let first=document.getElementById("dog2")
+let second=document.getElementById("cat2")
+let third=document.getElementById("dog")
+let four=document.getElementById("cat")
+
+second.style.display="none"
+
+first.style.display="block"
+
+
+})
+let mydiv5=document.getElementById("uniq2")
+mydiv5.addEventListener("click",()=>{
+
+let first=document.getElementById("dog2")
+let second=document.getElementById("cat2")
+
+second.style.display="block"
+
+first.style.display="none"
+
+
+})
+let mydiv7=document.getElementById("uniq1")
+mydiv7.addEventListener("click",()=>{
+
+let first=document.getElementById("dog3")
+let second=document.getElementById("cat3")
+
+second.style.display="none"
+first.style.display="block"
+
+})
+let mydiv8=document.getElementById("uniq2")
+mydiv8.addEventListener("click",()=>{
+
+let first=document.getElementById("dog3")
+let second=document.getElementById("cat3")
+
+second.style.display="block"
+
+first.style.display="none"
+
+
+})
